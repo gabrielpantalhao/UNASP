f6766409bb0ca9f347d70bc36dd227163ba672e4
Gabriel Pantalhão <gabriel.l.pantalhao@gmail.com>
2025-03-20 19:04:24 -0300

    mexendo com dicionário

--- Python/script.py
+++ Python/script.py
@@ -3,5 +3,13 @@
 # print(y)
 
 
-d = {"x": 2, "y": True, "z": "unasp"} # criar um dicionário onde cada chave é composto por um valor seguido de ":"
-print(d) # exibir o dicionário criado
+# d = {"x": 2, "y": [True, False], "z": {1, 2, 3}} # criar um dicionário onde cada chave é composto por um valor seguido de ":"
+# print(d["y"][0]) # exibir o dicionário criado
+
+tabela = {"Alface": 6.00,
+          "Tomate": 8.00,
+          "Batata": 6.00,}
+
+i = input("Digite o nome de um produto: ")
+
+print(tabela.get(i))
