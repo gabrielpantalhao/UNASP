489b03d2c175ad02a16596ea7121ef4843dda325
Gabriel Pantalhão <gabriel.l.pantalhao@gmail.com>
2025-03-20 20:13:51 -0300

    exercício de supermercado com python e dicionátio

--- Python/script.py
+++ Python/script.py
@@ -8,8 +8,16 @@
 
 tabela = {"Alface": 6.00,
           "Tomate": 8.00,
-          "Batata": 6.00,}
+          "Batata": 6.00,
+          "Carne": 39.00,
+          "Cenoura": 7.90
+           }
 
-i = input("Digite o nome de um produto: ")
-
-print(tabela.get(i))
+produto = input("Digite o nome de um produto: ") # solicitar o nome de um produto
+quanti = float(input("Qual a quantidade? ")) # converter a entrada para float
+prodFinal = float(tabela.get(produto) * quanti)
+pagamento = float(input(f"Total a pagar será de R$ {prodFinal:.2f}, Qual será a quantidade a ser paga? "))
+if pagamento >= prodFinal:
+    print(f"Pagamento efetuado com sucesso! Seu troco é de R${pagamento - prodFinal:.2f}")
+elif pagamento < prodFinal:
+    print(f"Pagamento insuficiente! Você precisa pagar R${prodFinal - pagamento:.2f}")
