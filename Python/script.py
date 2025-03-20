dbdf54cac2ec255ba843977908fc6f601ebdf171
Gabriel Pantalhão <gabriel.l.pantalhao@gmail.com>
2025-03-19 22:07:38 -0300

    primeito commit

---
+++ Python/script.py
@@ -0,0 +1,7 @@
+# x = [1, 2, 2, 3, 3]
+# y = list(set(x)) # remove itens duplicados da variável x
+# print(y)
+
+
+d = {"x": 2, "y": True, "z": "unasp"} # criar um dicionário onde cada chave é composto por um valor seguido de ":"
+print(d) # exibir o dicionário criado
